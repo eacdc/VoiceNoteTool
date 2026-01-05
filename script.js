e8d8d1c1fc4f09b66c3df14192400dd7a1b8b3a6
eacdc <ea@cdcprinters.com>
2026-01-05 11:38:12 +0530

    change

--- script.js
+++ script.js
@@ -765,7 +765,8 @@ if (!isLoginPage) {
             toDepartment,
             audioBlob: base64Audio,
             audioMimeType: audioBlob.type,
-            createdBy: username
+            createdBy: username,
+            summary: audioSummary || ''
           };
 
           await voiceNoteToolAPI.saveAudio(audioData);
